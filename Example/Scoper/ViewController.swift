97f99065da61ad2d397a4db8506c3a4a731f56b7
Petro Korienev <soxjke@gmail.com>
2018-04-07 13:17:37 +0300

    Initial commit

---
+++ Example/Scoper/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Scoper
+//
+//  Created by soxjke on 04/07/2018.
+//  Copyright (c) 2018 soxjke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
